61ca4dd834fd03e666efc9eaab18a5977de09259
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-05-29 15:11:08 +0200

    layout page done

--- Zubrs.Mvc/Controllers/NavController.cs
+++ Zubrs.Mvc/Controllers/NavController.cs
@@ -1,12 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Zubrs.Mvc.Models;
 
 namespace Zubrs.Mvc.Controllers
 {
     public class NavController : Controller
     {
+        private readonly Lazy<IEnumerable<MenuItem>> menuLazy = new Lazy<IEnumerable<MenuItem>>(CreateMenu);
+
         public PartialViewResult TopBar()
         {
-            return PartialView();
+            return PartialView(menuLazy.Value);
+        }
+
+        public PartialViewResult BottomBar()
+        {
+            return PartialView(menuLazy.Value);
+        }
+
+        private static IEnumerable<MenuItem> CreateMenu()
+        {
+            yield return new MenuItem { Title = "Новости", Controller = "news" };
+            yield return new MenuItem { Title = "Команды", Controller = "teams" };
+            yield return new MenuItem { Title = "Соревнования", Controller = "competitions" };
         }
     }
 }
---
+++ Zubrs.Mvc/Models/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Zubrs.Mvc.Models
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Controller { get; set; }
+        public bool IsActive { get; set; }
+        public MenuItem[] SubItems { get; set; }
+    }
+}
