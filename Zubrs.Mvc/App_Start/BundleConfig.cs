da94a6cd61df3606aca311d049c00e3568b6f158
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-06-24 22:44:44 +0200

    NSass package removed

--- Zubrs.Mvc/App_Start/BundleConfig.cs
+++ Zubrs.Mvc/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
 using System.Web.Optimization;
-using NSass;
 
 namespace Zubrs.Mvc
 {
@@ -13,9 +12,9 @@ namespace Zubrs.Mvc
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                 "~/Scripts/modernizr-*"));
 
-            bundles.Add(new SassBundle("~/bundles/css").Include(
-                "~/sass/normalize.scss",
-                "~/sass/site.scss"));
+            //bundles.Add(new SassBundle("~/bundles/css").Include(
+            //    "~/sass/normalize.scss",
+            //    "~/sass/site.scss"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
