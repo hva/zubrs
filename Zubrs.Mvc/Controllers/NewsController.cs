430480686def04d1ea5e9da1ad84d2f47445d465
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-07-06 20:32:06 +0200

    news - view started

--- Zubrs.Mvc/Controllers/NewsController.cs
+++ Zubrs.Mvc/Controllers/NewsController.cs
@@ -1,18 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Ninject;
+using Zubrs.Data;
 using Zubrs.Mvc.Infrastructure;
 
 namespace Zubrs.Mvc.Controllers
 {
     public class NewsController : Controller
     {
+        [Inject]
+        public IDataRepository Repository { get; set; }
+
         public NewsController(MenuManager menu)
         {
             menu.CurrentRouteName = RouteName.News;
         }
 
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var model = await Repository.Articles.ToArrayAsync();
+            return View(model);
         }
     }
 }
