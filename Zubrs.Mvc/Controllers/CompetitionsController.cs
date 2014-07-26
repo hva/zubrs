463aabc72026d515314eb8d60fd63ef9847ffca1
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-07-26 13:39:13 +0200

    competitions - view model started

--- Zubrs.Mvc/Controllers/CompetitionsController.cs
+++ Zubrs.Mvc/Controllers/CompetitionsController.cs
@@ -1,18 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Ninject;
 using Zubrs.Mvc.Infrastructure;
+using Zubrs.Mvc.ViewModels;
 
 namespace Zubrs.Mvc.Controllers
 {
     public class CompetitionsController : Controller
     {
+        [Inject]
+        public CompetitionViewModel ViewModel { get; set; }
+
         public CompetitionsController(MenuManager menuManager)
         {
             menuManager.CurrentRouteName = RouteName.Competitions;
         }
 
-        public ActionResult Index(int? id)
+        public async Task<ActionResult> Index(int? id)
         {
-            return View();
+            await ViewModel.InitAsync(id);
+            return View(ViewModel);
         }
     }
 }
---
+++ Zubrs.Mvc/ViewModels/CompetitionViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Ninject;
+using Zubrs.Data;
+using Zubrs.Models;
+
+namespace Zubrs.Mvc.ViewModels
+{
+    public class CompetitionViewModel
+    {
+        [Inject]
+        public IDataRepository Repository { get; set; }
+
+        public Competition[] Competitions { get; set; }
+        public Competition Competition { get; set; }
+
+        public async Task InitAsync(int? id)
+        {
+            Competitions = await Repository.Competitions.ToArrayAsync();
+            Competition = id.HasValue
+                ? Competitions.First(x => x.Id == id)
+                : Competitions[0];
+        }
+    }
+}
