90831840c72c4ae74ebf789bf77dcb3093d752c2
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-08-08 14:45:38 +0200

    yandex metrica removed

--- Zubrs.Mvc/Scripts/_references.js
+++ Zubrs.Mvc/Scripts/_references.js
@@ -3,5 +3,5 @@
 /// <reference path="modernizr-2.7.2.js" />
 /// <reference path="foundation/foundation.js" />
 /// <reference path="foundation/foundation.orbit.js" />
-/// <reference path="foundation/foundation.topbar.js" />
 /// <reference path="foundation/foundation.tab.js" />
+/// <reference path="foundation/foundation.topbar.js" />
