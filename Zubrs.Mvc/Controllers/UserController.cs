4acefbf93b553920ebd313042c0f258a972ecd4a
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-08-08 18:01:43 +0200

    login form UI

--- Zubrs.Mvc/Controllers/UserController.cs
+++ Zubrs.Mvc/Controllers/UserController.cs
@@ -6,15 +6,26 @@ using Zubrs.Mvc.ViewModels;
 
 namespace Zubrs.Mvc.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         [Inject]
         public MenuManager MenuManager { get; set; }
 
+        [AllowAnonymous]
         public async Task<ActionResult> Login()
         {
             await MenuManager.InitAsync(null);
             return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel vm)
+        {
+            await MenuManager.InitAsync(null);
+            return View(vm);
         }
     }
 }
--- Zubrs.Mvc/ViewModels/LoginViewModel.cs
+++ Zubrs.Mvc/ViewModels/LoginViewModel.cs
@@ -4,10 +4,11 @@ namespace Zubrs.Mvc.ViewModels
 {
     public class LoginViewModel
     {
-        [Required]
         [Display(Name = "Логин")]
         public string Login { get; set; }
 
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
